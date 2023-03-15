feb437d32a1a0f3d2627db5d140a45148701d8ee
Alice <alice.karatchentzeff@ucdconnect.ie>
2023-03-15 15:27:21 +0100

    Debugged and tested the input_tasks function and edit function. The file contains a main and print_function for testing purposes. Headers to be transfered to a header file.

--- inputTasks.c
+++ inputTasks.c
@@ -1,32 +1,97 @@
 #include <stdio.h>
-#include "main.c"
+#include <string.h>
 #define NAME_LEN 30
 
-void input_task(struct task Task, int num_tasks);
-void edit(struct task Tasks[], int num_tasks);
-void print_task(struct task Task);
+struct Task {
+    char name[NAME_LEN];
+    int start_month;
+    int end_month;
+    int num_dependences;
+    int dependences[10];
+};
 
-void input_task(struct task Task, int num_tasks)
+void input_task(struct Task *task, int num_tasks, int task_index);
+void edit(struct Task tasks[], int num_tasks);
+void print_task(struct Task task, int task_index);
+
+int main(void)
 {
+    struct Task user_task[10], test_data[10];
+    struct Task *ptr_task;
+    int num_tasks;
+    int choice;
+    printf("How many tasks do you wish to add?\n");
+    scanf("%d", &num_tasks);
+    while(num_tasks < 1)
+    {
+        printf("You must enter at least one task.\n");
+        scanf("%d", &num_tasks);
+    }
+
+    for(int i = 0; i < num_tasks; i++)
+    {
+        ptr_task = &user_task[i];
+        input_task(ptr_task, num_tasks, i);
+    }
+    printf("\n");
+
+    printf("Here are the tasks you entered:\n");
+    for(int i = 0; i < num_tasks; i++)
+    {
+        print_task(user_task[i], i);
+    }
+
+    while(1)
+    {
+        printf("Type '1' to edit a task and '2' to exit the program.\n");
+        scanf("%d", &choice);
+        while(choice < 1 || choice > 2)
+        {
+            printf("Please enter a number between 1 and 2.\n");
+            scanf("%d", &choice);
+        }
+        if(choice == 1)
+        {
+            edit(user_task, num_tasks);
+        }
+        else
+        {
+            return 0;
+        }
+    }
+
+    return 0;
+}
+
+
+void input_task(struct Task *task, int num_tasks, int task_index)
+{
+    //for readability, task index si converted to the task number and not its position in the array
+    task_index++;
+
     //taks name
-    printf("Please enter a name for the task:\n")
-    fgets(Task.name, NAME_LEN, stdin);
+    printf("Task %d name:\n", task_index);
+    fgets(task->name, NAME_LEN, stdin);
+    while(task->name[0] == '\n' || task->name[0] == ' ' || task->name[0] == '\t')
+    {
+        fgets(task->name, NAME_LEN, stdin);
+    }
     
     //tast starting month
     printf("Please enter a starting month (1-12):\n");
-    scanf("%d", &Task.start_month);
-    while(Task.start_month < 1 || Task.start_month > 12)
+    scanf("%d", &task->start_month);
+    while(task->start_month < 1 || task->start_month > 12)
     {
         printf("The month should be indicated with a number between 1 and 12. Please enter again:\n");
-        scanf("%d", &Task.start_month);
+        scanf("%d", &task->start_month);
     }
 
     //task ending month
     printf("Please enter an ending month (1-12):\n");
-    scanf("%d", &Task.end_month);
-    while(Task.end_month < 1 || Task.end_month > 12 || Task.end_month < Task.start_month)
+    scanf("%d", &task->end_month);
+    while(task->end_month < 1 || task->end_month > 12 || task->end_month < task->start_month)
     {
-        if(Task.end_month < Task.start_month)
+        if(task->end_month < task->start_month)
         {
             printf("The end month cannot happen before the start month. ");
         }
@@ -35,56 +100,84 @@ void input_task(struct task Task, int num_tasks)
             printf("The month should be indicated with a number between 1 and 12. ");
         }
         printf("Please enter again:\n");
-        scanf("%d", &Task.end_month);
+        scanf("%d", &task->end_month);
+    }
+
+    //task number of dependences
+    printf("Please enter the number of dependences:\n");
+    scanf("%d", &task->num_dependences);
+    while(task->num_dependences < 0 || task->num_dependences > (num_tasks - 1))
+    {
+        printf("The number of dependences must be comprised between 0 and %d. Please enter again:\n", num_tasks - 1);
+        scanf("%d", &task->num_dependences);
     }
 
     //task dependences
-    printf("Please enter the number of dependences:\n")
-    scanf("%d", &Task.num_dependences);
-    while(Task.num_dependences < 0 || > 9)
+    for(int i = 0; i < task->num_dependences; i++)
     {
-        printf("The number of dependences must be comprised between 0 and 9. Please enter again:\n");
-        scanf("%d", &Task.num_dependences);
-    }
-    for(int i = 0; i < Task.num_dependences; i++)
-    {
-        printf("Dependency %d (number):\n", i+1);
-        scanf("%d", & Task.dependences[i]);
-        while(Task.num_dependences[i] < 1 || > num_tasks)
+        printf("Dependency %d (please indicate the task number):\n", i+1);
+        scanf("%d", &task->dependences[i]);
+        while(task->dependences[i] < 1 || task->dependences[i] > num_tasks || task->dependences[i] == task_index)
         {
-            printf("The task number must be comprised between 1 and %d. Please enter again:\n", num_tasks);
-            scanf("%d", & Task.dependences[i]);
+            if(task->dependences[i] < 1 || task->dependences[i] > num_tasks)
+            {
+                printf("The task number must be between 1 and %d. ", num_tasks);
+            }
+            else
+            {
+                printf("The task cannot be its own dependency! ");
+            }
+            printf("Please enter again:\n");
+            scanf("%d", &task->dependences[i]);
         }
     }
 }
 
 
-void edit(struct task Tasks[], int num_tasks)
+void edit(struct Task tasks[], int num_tasks)
 {
-    char task_name;
+    char task_name[NAME_LEN];
     printf("Please enter the name of the task to modify:\n");
     fgets(task_name, NAME_LEN, stdin);
-    for(int i = 0; i < num_tasks; i++){
-        if(strcmp(Tasks[i].name, task_name) == 0)
+    if(task_name[0] != '\n')
+    {
+        for(int i = 0; i < num_tasks; i++)
         {
-            printf("Previous version of this task:\n")
-            print_task(Task[i]);
-            input_task(Task[i], num_tasks);
-            display_gantt(num_tasks, task Tasks);
+            if(strcmp(tasks[i].name, task_name) == 0)
+            {
+                printf("Previous version of this task:\n");
+                print_task(tasks[i], i);
+                input_task(&tasks[i], num_tasks, i);
+                printf("New version of the task:\n");
+                print_task(tasks[i], i);
+                /*
+                display_gantt(num_tasks, tasks[]);
+                */
+            break;
+            }
         }
     }
+    else
+    {
+        printf("Operation cancelled.\n");
+    }    
 }
 
 
-void print_task(struct task Task)
+void print_task(struct Task task, int task_index)
 {
-    printf("Task name: %s\n", Task.name);
-    printf("Sarting month: %d\n", Task.start_month);
-    printf("Ending month: %d\n", Task.start_month);
-    printf("Dependencies: ");
-    for(int i = 0; i < Task.num_dependences; i++)
+    task_index++;
+
+    printf("Task %d name: %s", task_index, task.name);
+    printf("Sarting month: %d\n", task.start_month);
+    printf("Ending month: %d\n", task.end_month);
+    if(task.num_dependences > 0)
     {
-        printf("%d ", Task.dependences[i]);
+        printf("Dependencies: ");
+        for(int i = 0; i < task.num_dependences; i++)
+        {
+            printf("%d ", task.dependences[i]);
+        }   
     }
-    printf("\n");
+    printf("\n\n");
 }
