48d3e68c97adee4d261a3009527dc8e77a230a32
Alice <alice.karatchentzeff@ucdconnect.ie>
2023-03-20 13:42:34 +0100

    added function delcarations and fixed incorrect call to check_dependencies

--- check_dependencies.c
+++ check_dependencies.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include "inputTasks.h"
 
+void test(struct Task tasks[], int num_tasks);
+int check_dependencies(struct Task tasks[], struct Task task, int task_index);
+
 /*This method examines the task dependencies recursively and prints whether there is a circular dependency or not.
  * */
 void test(struct Task tasks[], int num_tasks)
@@ -25,7 +28,7 @@ int check_dependencies(struct Task tasks[], struct Task task, int task_index)
     {
         //base case 2: the task's dependency is the same as the task being checked'
         //recursive case: compare the task index at hand with the dependencies of its dependencies
-        if(task.dependencies[i] == task_index || check_dependencies(tasks[task.dependencies[i]], task_index) == 1)
+        if(task.dependencies[i] == task_index || check_dependencies(tasks, tasks[task.dependencies[i]], task_index) == 1)
         {
             return 1;
         }
